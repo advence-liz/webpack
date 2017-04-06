dce8d18f1f213a0edb9b002d1ae83b34415dc23a
advence-liz <1012515183@qq.com>
2017-04-06 22:40:30 +0800

    升级 webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,29 +10,42 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.less/, loader: 'style-loader!css-loader!less-loader' },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                // query:
-                // {
-                //     presets: ['react', 'es2015']
-                // }
-            }
-        ]
+        // loaders: [
+        //     { test: /\.css$/, loader: "style!css" },
+        //     { test: /\.less/, loader: 'style-loader!css-loader!less-loader' },
+        //     {
+        //         test: /\.js$/,
+        //         exclude: /node_modules/,
+        //         loader: "babel-loader",
+        //         query:
+        //         {
+        //             presets: ['react', 'es2015']
+        //         }
+        //     }
+        // ],
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [['es2015', { modules: false }],'react']
+                    //plugins: ['syntax-dynamic-import']
+                }
+            }]
+        }]
+
     },
-    babel: {
-       presets: ['react', 'es2015']
-    },
+    // babel: {
+    //    presets: ['react', 'es2015']
+    // },
     devtool: "source-map",
     resolve: {
-        extensions: ['', '.js', '.json', '.jsx']
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]
+        extensions: ['.js', '.json', '.jsx']
+    }
+    // ,
+    // plugins: [
+    //     new webpack.NoErrorsPlugin(),
+    //     new webpack.HotModuleReplacementPlugin()
+    // ]
 };
