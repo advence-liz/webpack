af3a366c4d3731a92be9d46d19f58354183fe7b9
advence-liz <1012515183@qq.com>
2017-04-11 11:18:02 +0800

    修改webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 var pkg = require("./package"),
     options = process.argv,
     config;
@@ -5,6 +6,7 @@ var pkg = require("./package"),
 /**
  * pkg.pattern {dev|prod} 原计划package.json 中配置是否为product 目前没有使用
  */
+
 console.log(options);
 
 var env = options[2].slice(6);
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+//var webpack = require('webpack');
 var path = require("path");
 
 
