85401b9adf81e6a2f408d705da97594aa6466450
advence-liz <1012515183@qq.com>
2017-07-05 21:51:01 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
  //import React from 'react.js';
 // import ReactDOM from 'react-dom';
 //import 'babel-polyfill';
-//import a ,{fun,d}from "./a";
+import a ,{fun,d}from "./a";
 const s = new Set();
 
 [2, 3, 5, 4, 5, 2, 2].forEach(x => s.add(x));
@@ -9,12 +9,12 @@ const s = new Set();
 for (let i of s) {
   console.log(i);
 }
-var load = require("bundle-loader?lazy&name=[name]!./aa/a.js");
+// var load = require("bundle-loader?lazy&name=[name]!./aa/a.js");
 
 // 只有在调用load的时候才会真正加载
-load(function(a){ 
-console.log(a);
-})
+// load(function(a){ 
+// console.log(a);
+// })
 
 
 
