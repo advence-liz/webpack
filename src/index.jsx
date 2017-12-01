74c3bf91108dc1a9be49ea521ab91cad27f47ad5
advence-liz <1012515183@qq.com>
2017-12-01 17:31:46 +0800

    update

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+
+console.log(process);
+console.log(global);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
