065899bfddb1ec5b6b27e2a7d3fd377313b10a95
advence-liz <1012515183@qq.com>
2018-07-25 19:31:03 +0800

    add: babel-useBuiltIns

---
+++ v4/babel-useBuiltIns/src/index.js
@@ -0,0 +1,8 @@
+let liz = 'liz'
+
+// delete String.prototype.includes
+console.log(liz.includes)
+// require('babel-polyfill')
+
+console.log(liz.includes('z'))
+console.log(liz.includes)
---
+++ v4/babel-useBuiltIns/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/',
+  mode: 'development',
+  context: __dirname,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: { modules: true }
+          },
+          {
+            loader: 'less-loader' // compiles Less to CSS
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(art|ejs)$/,
+        loader: 'art-template-loader',
+        options: {
+          compileDebug: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.less', '.scss']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'template/_layout.ejs',
+      favicon: 'template/favicon.ico',
+      // inject: false,
+      title: 'webpack'
+    })
+  ]
+}
