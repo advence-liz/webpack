038d0f432b791fb1a7e8c9ddff2d5b5de5f7db00
advence-liz <1012515183@qq.com>
2018-07-31 22:02:33 +0800

    test:polyfill

--- v4/babel-useBuiltIns/src/index.js
+++ v4/babel-useBuiltIns/src/index.js
@@ -1,8 +1,12 @@
 let liz = 'liz'
 
-// delete String.prototype.includes
+delete String.prototype.includes
+// delete Object.prototype.assign
+// delete Object.assign
+console.log(Object.assign)
 console.log(liz.includes)
-// require('babel-polyfill')
+require('babel-polyfill')
 
 console.log(liz.includes('z'))
 console.log(liz.includes)
+console.log(Object.assign({ v: 1 }, { cc: 2 }))
