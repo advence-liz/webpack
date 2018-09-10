e81c7702ae4b2cc1acf77e65e19bd713d711a302
advence-liz <1012515183@qq.com>
2018-09-10 15:45:36 +0800

    add:new time create

--- v4/react-redux-router/src/components/time-create/index.jsx
+++ v4/react-redux-router/src/components/time-create/index.jsx
@@ -1,87 +1,123 @@
 import React from 'react'
-import { Modal, Row, Col } from 'antd'
-import ReasonGroup from './reason-group'
-import ParentValidation from './parent-validation'
-import PropTypes from 'prop-types'
-export default class Absence extends React.Component {
+import { Checkbox, Row, Col, Icon } from 'antd'
+
+export default class TimeCreate extends React.Component {
   static defaultProps = {
-    options: {
-      time_from: '2018-09-10 10:00:00',
-      course: {
-        title: '朗文试听课-我的宠物'
+    grid: [
+      // 日程数据
+      {
+        date: '2018-09-01',
+        week_day: '星期一',
+        periods: [
+          // 含当天所有时段
+          {
+            time_from: '08:00', // 开始时间
+
+            time_to: '09:00', // 截止时间
+
+            checked: true // 是否可上课
+          },
+
+          {
+            time_from: '09:00', // 开始时间
+
+            time_to: '10:00', // 截止时间
+
+            checked: false // 是否可上课
+          }
+        ]
+      },
+      {
+        date: '2018-09-02',
+        week_day: '星期一',
+        periods: [
+          // 含当天所有时段
+          {
+            time_from: '08:00', // 开始时间
+
+            time_to: '09:00', // 截止时间
+
+            checked: true // 是否可上课
+          },
+
+          {
+            time_from: '09:00', // 开始时间
+
+            time_to: '10:00', // 截止时间
+
+            checked: false // 是否可上课
+          }
+        ]
       }
-    },
-    visible: true,
-    onCancel () {
-      console.log('cancle')
-    },
-    onOK (...rest) {
-      console.log(rest)
-    }
+    ]
   }
-  state = { isPass: false, reason: '', isInit: true }
-  onReasonChange = reason => {
-    this.setState({ reason })
-  }
-  onValidationChange = isPass => {
-    this.setState({ isPass, isInit: false })
-  }
-  onOKClick = () => {
-    let { isPass, isInit } = this.state
-    let { options, onOK } = this.props
-    isInit && this.setState({ isInit: false })
-    if (!isPass) {
-      return
-    }
-    onOK(this.state, options)
+  onCellClick = event => {
+    const { grid } = this.props
+    const {
+      target: { dataset }
+    } = event
+    let { period, dateIndex, periodIndex } = dataset
+    let currentPeriod = grid[(dateIndex, periodIndex)]
+    currentPeriod.checked = !currentPeriod.checked
   }
   render () {
-    const { options } = this.props
-    const { time_from: absenceTime, course } = options
-    const { title: asbenceCourse } = course || {}
+    const { grid } = this.props
+    const timePeriods = grid[0]['periods']
     return (
       <div>
-        <Modal
-          title="我要请假"
-          visible={this.props.visible}
-          onOk={this.onOKClick}
-          onCancel={this.props.onCancel}
-          okText="确认"
-          cancelText="取消"
-        >
-          <Row>
-            <Col span={6}>请假时间</Col>
-            <Col span={10}>{absenceTime}</Col>
-          </Row>
-          <Row>
-            <Col span={6}>请假课程</Col>
-            <Col span={10}>{asbenceCourse}</Col>
-          </Row>
-          <Row>
-            <Col span={6}>请假原因</Col>
-            <Col span={10}>
-              <ReasonGroup onReasonChange={this.onReasonChange} />
-            </Col>
-          </Row>
-          <Row style={{ marginTop: 20 }}>
-            <Col span={6}>家长验证</Col>
-            <Col span={18}>
-              <ParentValidation
-                isInit={this.state.isInit}
-                isPass={this.state.isPass}
-                onValidationChange={this.onValidationChange}
-              />
-            </Col>
-          </Row>
-        </Modal>
+        <Row className="grid-head">
+          <Col span={3}>
+            <Icon type="left-square" theme="outlined" />
+          </Col>
+          {grid.map((item, index) => {
+            const { date, week_day: weekDay } = item
+            return (
+              <Col span={3} key={`grid-head-${index}`}>
+                <Checkbox> {`${date.slice(-5)} ${weekDay}`}</Checkbox>
+                {index === grid.length - 1 && (
+                  <Icon type="right-square" theme="outlined" />
+                )}
+              </Col>
+            )
+          })}
+        </Row>
+        <Row className="grid-body">
+          <Col span={3}>
+            {timePeriods.map((item, index) => {
+              return (
+                <div style={{ height: 45 }} key={`time-period-${index}`}>
+                  {item.time_from}
+                </div>
+              )
+            })}
+          </Col>
+          {grid.map((item, dateIndex) => {
+            const { date, periods } = item
+            return (
+              <Col span={3} key={`grid-col-${date}`} onClick={this.onCellClick}>
+                {periods.map((period, periodIndex) => {
+                  const { checked } = period
+                  return (
+                    <div
+                      key={`${date}-${periodIndex}`}
+                      data-period={JSON.stringify({ ...period, date })}
+                      data-date-index={dateIndex}
+                      data-period-index={periodIndex}
+                      style={{
+                        background: checked ? '#0CC499' : '#fff',
+                        height: 45,
+                        border: '1px solid #E5E5E5'
+                      }}
+                    >
+                      {period.time_from}
+                    </div>
+                  )
+                })}
+              </Col>
+            )
+          })}
+        </Row>
       </div>
     )
   }
-}
-
-Absence.propTypes = {
-  options: PropTypes.object.isRequired,
-  visible: PropTypes.bool.isRequired,
-  onOK: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired
 }
